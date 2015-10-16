be93932fa9076990298be27721011e45eff23de6
Etsuji Nakai <enakai@redhat.com>
2015-10-16 12:30:49 +0900

    Refactor for NikkeiLinux

--- tapinfo.py
+++ tapinfo.py
@@ -4,37 +4,37 @@
 #
 #   2010/01/20 ver1.0
 #   2010/01/21 ver1.1
+#   2015/10/16 ver1.2
 #
 
-import libvirt
-import os
-import re
+#!/usr/bin/python
+
+import libvirt, os, re
 from xml.dom import minidom
 
-Conn = libvirt.open( "qemu:///system" )
+Conn = libvirt.open("qemu:///system")
 
 def showTaps():
-    vnetRe = re.compile( "vnet\d+" )
+    vnetRe = re.compile("vnet\d+")
     fmt = "%-16s %-12s %-18s %-12s %-12s"
-    print fmt % ( "Domain", "Tap", "MAC Address", "Network", "Bridge" )
+    print fmt % ("Domain", "Tap", "MAC Address", "Network", "Bridge")
     for id in Conn.listDomainsID():
         print "-" * 78
-        vm = Conn.lookupByID( id )
-        vmXMLDesc = minidom.parseString( vm.XMLDesc( 0 ) )
-        for iface in vmXMLDesc.getElementsByTagName( "interface" ):
-            ifaceType = iface.getAttribute( "type" )
+        vm = Conn.lookupByID(id)
+        vmXMLDesc = minidom.parseString(vm.XMLDesc(0))
+        for iface in vmXMLDesc.getElementsByTagName("interface"):
+            ifaceType = iface.getAttribute("type")
             if ifaceType == "network":
-                network = iface.getElementsByTagName( "source" )[0].getAttribute( "network" )
-                netXMLDesc = minidom.parseString( Conn.networkLookupByName( network ).XMLDesc( 0 ) )
-                bridge = netXMLDesc.getElementsByTagName( "bridge" )[ 0 ].getAttribute( "name" )
+                network = iface.getElementsByTagName("source")[0].getAttribute("network")
+                netXMLDesc = minidom.parseString(Conn.networkLookupByName(network).XMLDesc(0))
+                bridge = netXMLDesc.getElementsByTagName("bridge")[0].getAttribute("name")
             elif ifaceType == "bridge":
                 network = ""
-                bridge = iface.getElementsByTagName( "source" )[0].getAttribute( "bridge" )
-            mac = iface.getElementsByTagName( "mac" )[0].getAttribute( "address" )
-            device = iface.getElementsByTagName( "target" )[0].getAttribute( "dev" )
-            print fmt % ( vm.name(), device, mac, network, bridge )
+                bridge = iface.getElementsByTagName("source")[0].getAttribute("bridge")
+            mac = iface.getElementsByTagName("mac")[0].getAttribute("address")
+            device = iface.getElementsByTagName("target")[0].getAttribute("dev")
+            print fmt % (vm.name(), device, mac, network, bridge)
 
 if __name__ == "__main__":
     showTaps()
     print
-
